20a7a8b6f4e3f15d4826d36a19f50862c74fc353
Ербулатов Хамит <90035385+Hamsike2021@users.noreply.github.com>
2021-10-22 13:08:27 +0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,15 @@
+s1 = ['а', 'о', 'у', 'ы', 'э', 'я', 'ё', 'ю', 'и', 'е']
+def glas(s): # для количества гласных
+  k = 0
+  for el in s:
+    if el in s1:
+      k += 1
+  return k
+def sogl(s): # для количества согласных
+  k = 0
+  for el in s:
+    if el not in s1:
+      k += 1
+  return k
+s = input().lower()
+print(glas(s))
